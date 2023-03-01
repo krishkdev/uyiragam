0665f5c3cc07eec215697644d19466f186af102f
Krishnakumar V <krishkumarv2001@gmail.com>
2023-03-01 17:21:19 +0530

    Checkout redesign : phase 1

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -13,10 +13,12 @@ import { urlFor } from "../lib/client";
 import { CartItem } from "@/models/CartItem";
 import Image from "next/image";
 import Modal from "../components/Modal";
+import { useRouter } from "next/router";
 
 export const Cart = () => {
+  const router = useRouter();
   const cartRef = useRef<HTMLElement>();
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
   const {
     totalPrice,
     totalQuantities,
@@ -114,6 +116,7 @@ export const Cart = () => {
               );
             })}
         </div>
+
         {cartItems.length >= 1 && (
           <div className={styles.cart_bottom}>
             <div className={styles.total}>
@@ -124,14 +127,17 @@ export const Cart = () => {
               <button
                 type="button"
                 className={styles.btn}
-                onClick={() => setIsOpen(true)}
+                onClick={() => {
+                  router.push('/checkout')
+                  setShowCart(false);
+                }}
               >
                 Checkout
               </button>
             </div>
           </div>
         )}
-        {isOpen && <Modal setIsOpen={setIsOpen} />}
+        {/* {isOpen && <Modal setIsOpen={setIsOpen} />} */}
       </div>
     </div>
   );
---
+++ components/forms/UserForm.tsx
@@ -0,0 +1,59 @@
+import { FormWrapper } from "./FormWrapper"
+
+type UserData = {
+  firstName: string
+  lastName: string
+  phone: string
+}
+
+type UserFormProps = UserData & {
+  updateFields: (fields: Partial<UserData>) => void
+}
+
+export function UserForm({
+  firstName,
+  lastName,
+  phone,
+  updateFields,
+}: UserFormProps) {
+  return (
+    <FormWrapper title="User Details">
+      <label>First Name</label>
+      <input
+        autoFocus
+        required
+        type="text"
+        value={firstName}
+        onChange={e => updateFields({ firstName: e.target.value })}
+      />
+      <label>Last Name</label>
+      <input
+        required
+        type="text"
+        value={lastName}
+        onChange={e => updateFields({ lastName: e.target.value })}
+      />
+      {/* <label>Age</label>
+      <input
+        required
+        min={1}
+        type="number"
+        value={age}
+        onChange={e => updateFields({ age: e.target.value })}
+      /> */}
+      <label>Phone No</label>
+      <div style ={{display:'flex'}}>
+      <select style ={{width: '25%'}}>
+        <option>+91</option>
+      </select>
+      <input
+        required
+        min={1}
+        type="number"
+        value={phone}
+        onChange={e => updateFields({ phone: e.target.value })}
+      />
+      </div>
+    </FormWrapper>
+  )
+}
